6607c392b540e4c061244984de2748651a8348d2
Hilan <hilan.paganini@gmail.com>
2025-01-04 15:53:53 -0300

    Criadas entidades

--- src/main/java/com/cursodevsuperior/dscommerce/DscommerceApplication.java
+++ src/main/java/com/cursodevsuperior/dscommerce/DscommerceApplication.java
@@ -1,19 +1,12 @@
 package com.cursodevsuperior.dscommerce;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DscommerceApplication implements CommandLineRunner{
+public class DscommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscommerceApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Estou executando...");
-	}
-
 }
